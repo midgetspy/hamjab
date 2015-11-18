9a5b3daca3dd8caff0ff9b54af430312e4fddbe7
Nic Wolfe <nic@wolfeden.ca>
2015-11-18 00:03:35 -0700

    Added more built in macros

--- lib.py
+++ lib.py
@@ -314,11 +314,28 @@ class MacroResource(ResourceBase):
 
     macroJson = """
 {
+    "startTheatre": {
+        "name": "Prepare theatre",
+        "commands": [
+            {"device": "epson5030ub", "command": "PWR ON"},
+            {"device": "lutrongrx3000", "command": ":A11"}
+        ]
+    },
     "enable3D": {
         "name": "Enable 3D",
         "commands": [
             {"device": "epson5030ub", "command": "KEY AA"},
             {"device": "epson5030ub", "command": "KEY 59"},
+            {"device": "epson5030ub", "command": "KEY 49"},
+            {"device": "epson5030ub", "command": "KEY 3C"},
+            {"device": "epson5030ub", "command": "KEY 9F"}
+        ]
+    },
+    "disable3D": {
+        "name": "Disable 3D",
+        "commands": [
+            {"device": "epson5030ub", "command": "KEY AA"},
+            {"device": "epson5030ub", "command": "KEY 58"},
             {"device": "epson5030ub", "command": "KEY 49"},
             {"device": "epson5030ub", "command": "KEY 3C"},
             {"device": "epson5030ub", "command": "KEY 9F"}
