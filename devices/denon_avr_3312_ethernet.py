1d9420056c40f9404a8976c7c2416ff15a6bf3c9
Nic Wolfe <nic@wolfeden.ca>
2015-12-04 16:25:04 -0700

    Added an ethernet device base class and a Denon AVR-3312 device which uses it for communication (untested).

---
+++ devices/denon_avr_3312_ethernet.py
@@ -0,0 +1,7 @@
+from devices.device_lib import EthernetDevice
+
+class Device(EthernetDevice):
+    """
+    An untested device for a Denon AVR-3312 over ethernet (and related receivers probably)
+    """
+    deviceId = 'denon_avr_3312'
--- devices/device_lib.py
+++ devices/device_lib.py
@@ -2,6 +2,7 @@ from lib import QueuedLineSender
 
 from twisted.internet import reactor
 from twisted.internet.serialport import SerialPort    
+from twisted.internet.endpoints import connectProtocol, TCP4ClientEndpoint
 
 class SerialDevice(QueuedLineSender):
     """
@@ -19,3 +20,29 @@ class SerialDevice(QueuedLineSender):
     def startConnection(self):
         # use the defaults which are: 9600 baud, no parity, 8 data bits and 1 stop bit
         SerialPort(self, self.com_port, reactor, **self.com_options)
+
+class EthernetDevice(QueuedLineSender):
+    """
+    A base class for communicating with a device through ethernet
+    
+    host_string: Should be IP or hostname and optionally also the port number in the format <host>:<port> (ie. 192.168.1.60:23)
+    """
+    
+    DEFAULT_PORT = 23
+    
+    def __init__(self, connection_string):
+        num_occurrences = connection_string.count(':')
+        if num_occurrences == 0:
+            self._host = connection_string
+            self._port = self.DEFAULT_PORT
+        elif num_occurrences == 1:
+            self._host, self._port = connection_string.split(':')
+            self._port = int(self._port)
+        else:
+            raise Exception("Invalid connection string {}".format(connection_string))
+
+        QueuedLineSender.__init__(self)
+    
+    def startConnection(self):
+        endpoint = TCP4ClientEndpoint(reactor, self._host, self._port)
+        connectProtocol(endpoint, self)
