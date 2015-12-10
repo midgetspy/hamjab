d9e88f7a5868962838da1eb0d6a7beaa549cbd0d
Nic Wolfe <nic@wolfeden.ca>
2015-12-09 23:12:11 -0700

    Update the AVR-3312 device module comment now that it's been tested.

--- devices/denon_avr_3312_ethernet.py
+++ devices/denon_avr_3312_ethernet.py
@@ -2,6 +2,7 @@ from devices.device_lib import EthernetDevice
 
 class Device(EthernetDevice):
     """
-    An untested device for a Denon AVR-3312 over ethernet (and related receivers probably)
+    A device module for a Denon AVR-3312 over ethernet. Also tested on Denon AVR-X4000. Refer
+    to the documentation for your particular receiver for differences in the commands.
     """
     deviceId = 'denon_avr_3312'
